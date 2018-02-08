1e2c561e5e23aee306de66eabac37687552effcf
Adrian Kleinikkink <Akleinikkink9587@conestogac.on.ca>
2018-02-08 11:22:28 -0500

    class file complete

--- QA1/Rectangle.cs
+++ QA1/Rectangle.cs
@@ -4,7 +4,55 @@ using System.Text;
 
 namespace QA1
 {
-    class Rectangle
+    public class Rectangle
     {
+        // rectangle width + length
+        private int rectLeng, rectWid;
+
+        // setup
+        public Rectangle()
+        {
+            // set base values
+            rectLeng = 1;
+            rectWid = 1;
+        }
+
+        // get length
+        public int GetLength()
+        {
+            return rectLeng;
+        }
+
+        // set length
+        public int SetLength(int length)
+        {
+            rectLeng = length;
+            return rectLeng;
+        }
+
+        // get width
+        public int GetWidth()
+        {
+            return rectWid;
+        }
+
+        // set  width
+        public int SetWidth(int width)
+        {
+            rectWid = width;
+            return rectWid;
+        }
+
+        // calc perimeter
+        public int GetPerimeter()
+        {
+            return rectLeng * 2 + rectWid * 2;
+        }
+
+        // calc area
+        public int GetArea()
+        {
+            return rectLeng * rectWid;
+        }
     }
 }
